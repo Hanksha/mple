d7e555ab9a018e6364b58f8412ce024701c8ad36
Vivien <bouclettevirile_1@hotmail.fr>
2016-08-23 21:46:05 +0800

    First commit

---
+++ src/test/groovy/com/hanksha/mpwe/MpweApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.hanksha.mpwe
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class MpweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
